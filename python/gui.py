6d88e5e22764f76ab5b04caf29d11f09e76f8cff
Dhanannjay Deo <dhandeo@gmail.com>
2016-10-24 18:00:35 -0400

    Respond to click event

--- python/gui.py
+++ python/gui.py
@@ -1,16 +1,30 @@
 import sys
-from PyQt4 import QtGui
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
 def window():
-   app = QtGui.QApplication(sys.argv)
-   w = QtGui.QWidget()
-   b = QtGui.QLabel(w)
-   b.setText("Hello World!")
-   w.setGeometry(100,100,200,50)
-   b.move(50,20)
-   w.setWindowTitle("Sudoku")
-   w.show()
+   app = QApplication(sys.argv)
+   win = QDialog()
+   b1 = QPushButton(win)
+   b1.setText("Button1")
+   b1.move(50,20)
+   b1.clicked.connect(b1_clicked)
+
+   b2 = QPushButton(win)
+   b2.setText("Button2")
+   b2.move(50,50)
+   QObject.connect(b2,SIGNAL("clicked()"),b2_clicked)
+
+   win.setGeometry(100,100,200,100)
+   win.setWindowTitle("PyQt")
+   win.show()
    sys.exit(app.exec_())
+
+def b1_clicked():
+   print "Button 1 clicked"
+
+def b2_clicked():
+   print "Button 2 clicked"
 
 if __name__ == '__main__':
    window()
